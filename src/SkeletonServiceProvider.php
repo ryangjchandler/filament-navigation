2a472342c91ccbffe560a6490fa4937258484011
Ryan Chandler <ryangjchandler@gmail.com>
2022-04-18 14:18:40 +0100

    Initial commit

---
+++ src/SkeletonServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace VendorName\Skeleton;
+
+use Filament\PluginServiceProvider;
+
+class SkeletonServiceProvider extends PluginServiceProvider
+{
+    public static string $name = 'skeleton';
+}

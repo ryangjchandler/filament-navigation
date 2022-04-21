5df7ba61ad1ab85b4aee9e79a166698705a0226d
Ryan Chandler <ryangjchandler@gmail.com>
2022-04-21 21:00:35 +0100

    chore: tidy up code

--- resources/views/components/nav-item.blade.php
+++ resources/views/components/nav-item.blade.php
@@ -1,8 +1,9 @@
 @props(['item', 'statePath', 'nested' => false, 'moveUp' => false, 'moveDown' => false, 'indent' => false])
 
-<div data-state-path="{{ $statePath }}"
+<div
     wire:key="{{ $statePath }}"
-    class="space-y-2">
+    class="space-y-2"
+>
     <div class="relative group">
         @if ($nested)
             <span class="absolute -ml-3 text-xs text-gray-300 -translate-y-1/2 top-1/2">
@@ -10,78 +11,92 @@
             </span>
         @endif
 
-        <button type="button"
+        <button
+            type="button"
             wire:click="editItem('{{ $statePath }}')"
             @class([
                 'appearance-none w-full bg-white rounded-lg border border-gray-300 px-3 py-2 text-left',
-                'dark:bg-gray-700 dark:border-gray-600' => config('forms.dark_mode'),
-            ])>
+                'dark:bg-gray-700 dark:border-gray-600' => config('filament.dark_mode'),
+            ])
+        >
             {{ $item['label'] }}
         </button>
 
         <div @class([
             'absolute top-0 right-0 h-6 divide-x rounded-bl-lg rounded-tr-lg border-gray-300 border-b border-l overflow-hidden rtl:border-l-0 rtl:border-r rtl:right-auto rtl:left-0 rtl:rounded-bl-none rtl:rounded-br-lg rtl:rounded-tr-none rtl:rounded-tl-lg hidden opacity-0 group-hover:opacity-100 group-hover:flex transition ease-in-out duration-250',
-            'dark:border-gray-600 dark:divide-gray-600' => config('forms.dark_mode'),
+            'dark:border-gray-600 dark:divide-gray-600' => config('filament.dark_mode'),
         ])>
-            <button x-init
+            <button
+                x-init
                 x-tooltip.raw="Add child"
                 type="button"
                 wire:click="addChild('{{ $statePath }}')"
                 class="p-1"
-                title="Add child">
+                title="Add child"
+            >
                 <x-heroicon-o-plus class="w-3 h-3 text-gray-500 hover:text-gray-900" />
             </button>
 
             @if ($moveUp)
-                <button x-init
+                <button
+                    x-init
                     x-tooltip.raw="Move up"
                     type="button"
                     wire:click="moveItemUp('{{ $statePath }}')"
                     class="p-1"
-                    title="Move up">
+                    title="Move up"
+                >
                     <x-heroicon-o-arrow-up class="w-3 h-3 text-gray-500 hover:text-gray-900" />
                 </button>
             @endif
 
             @if ($moveDown)
-                <button x-init
+                <button
+                    x-init
                     x-tooltip.raw="Move down"
                     type="button"
                     wire:click="moveItemDown('{{ $statePath }}')"
                     class="p-1"
-                    title="Move down">
+                    title="Move down"
+                >
                     <x-heroicon-o-arrow-down class="w-3 h-3 text-gray-500 hover:text-gray-900" />
                 </button>
             @endif
 
             @if ($indent)
-                <button x-init
+                <button
+                    x-init
                     x-tooltip.raw="Indent"
                     type="button"
                     wire:click="indentItem('{{ $statePath }}')"
                     class="p-1"
-                    title="Indent">
+                    title="Indent"
+                >
                     <x-heroicon-o-arrow-right class="w-3 h-3 text-gray-500 hover:text-gray-900" />
                 </button>
             @endif
 
             @if ($nested)
-                <button x-init
+                <button
+                    x-init
                     x-tooltip.raw="Dedent"
                     type="button"
                     wire:click="dedentItem('{{ $statePath }}')"
                     class="p-1"
-                    title="Dedent">
+                    title="Dedent"
+                >
                     <x-heroicon-o-arrow-left class="w-3 h-3 text-gray-500 hover:text-gray-900" />
                 </button>
             @endif
 
-            <button x-init
+            <button
+                x-init
                 x-tooltip.raw="Remove"
                 type="button"
                 wire:click="removeItem('{{ $statePath }}')"
                 class="p-1"
-                title="Remove">
+                title="Remove"
+            >
                 <x-heroicon-o-trash class="w-3 h-3 text-danger-500 hover:text-danger-900" />
             </button>
         </div>

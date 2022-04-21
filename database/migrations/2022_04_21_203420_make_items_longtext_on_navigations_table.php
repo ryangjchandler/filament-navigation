0725e3134ea21f1c0c4eadae9e8ecfd8a4d07422
Ryan Chandler <ryangjchandler@gmail.com>
2022-04-21 20:52:04 +0100

    fix: use longText instead of json column for MySQL 5.7

---
+++ database/migrations/2022_04_21_203420_make_items_longtext_on_navigations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('navigations', function (Blueprint $table) {
+            $table->longText('items')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('navigations');
+    }
+};

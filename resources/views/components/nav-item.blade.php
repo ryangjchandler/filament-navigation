2dfc6fb2be64c6a4ea61235c028a2c02ab20e52b
awcodes <adam@adamwgd.com>
2022-04-21 09:23:58 -0400

    Fixes item input styling for dark mode.

--- resources/views/components/nav-item.blade.php
+++ resources/views/components/nav-item.blade.php
@@ -1,25 +1,21 @@
-@props([
-    'item',
-    'statePath',
-    'nested' => false,
-    'moveUp' => false,
-    'moveDown' => false,
-    'indent' => false,
-])
+@props(['item', 'statePath', 'nested' => false, 'moveUp' => false, 'moveDown' => false, 'indent' => false])
 
-<div data-state-path="{{ $statePath }}" wire:key="{{ $statePath }}" class="space-y-2">
+<div data-state-path="{{ $statePath }}"
+    wire:key="{{ $statePath }}"
+    class="space-y-2">
     <div class="relative group">
-        @if($nested)
-            <span class="absolute top-1/2 -translate-y-1/2 text-xs -ml-3 text-gray-300">
+        @if ($nested)
+            <span class="absolute -ml-3 text-xs text-gray-300 -translate-y-1/2 top-1/2">
                 &ndash;
             </span>
         @endif
 
-        <button
-            type="button"
+        <button type="button"
             wire:click="editItem('{{ $statePath }}')"
-            class="appearance-none w-full bg-white rounded-lg border border-gray-300 px-3 py-2 text-left"
-        >
+            @class([
+                'appearance-none w-full bg-white rounded-lg border border-gray-300 px-3 py-2 text-left',
+                'dark:bg-gray-700 dark:border-gray-600' => config('forms.dark_mode'),
+            ])>
             {{ $item['label'] }}
         </button>
 
@@ -27,44 +23,80 @@
             'absolute top-0 right-0 h-6 divide-x rounded-bl-lg rounded-tr-lg border-gray-300 border-b border-l overflow-hidden rtl:border-l-0 rtl:border-r rtl:right-auto rtl:left-0 rtl:rounded-bl-none rtl:rounded-br-lg rtl:rounded-tr-none rtl:rounded-tl-lg hidden opacity-0 group-hover:opacity-100 group-hover:flex transition ease-in-out duration-250',
             'dark:border-gray-600 dark:divide-gray-600' => config('forms.dark_mode'),
         ])>
-            <button x-init x-tooltip.raw="Add child" type="button" wire:click="addChild('{{ $statePath }}')" class="p-1" title="Add child">
+            <button x-init
+                x-tooltip.raw="Add child"
+                type="button"
+                wire:click="addChild('{{ $statePath }}')"
+                class="p-1"
+                title="Add child">
                 <x-heroicon-o-plus class="w-3 h-3 text-gray-500 hover:text-gray-900" />
             </button>
 
-            @if($moveUp)
-                <button x-init x-tooltip.raw="Move up" type="button" wire:click="moveItemUp('{{ $statePath }}')" class="p-1" title="Move up">
+            @if ($moveUp)
+                <button x-init
+                    x-tooltip.raw="Move up"
+                    type="button"
+                    wire:click="moveItemUp('{{ $statePath }}')"
+                    class="p-1"
+                    title="Move up">
                     <x-heroicon-o-arrow-up class="w-3 h-3 text-gray-500 hover:text-gray-900" />
                 </button>
             @endif
 
-            @if($moveDown)
-                <button x-init x-tooltip.raw="Move down" type="button" wire:click="moveItemDown('{{ $statePath }}')" class="p-1" title="Move down">
+            @if ($moveDown)
+                <button x-init
+                    x-tooltip.raw="Move down"
+                    type="button"
+                    wire:click="moveItemDown('{{ $statePath }}')"
+                    class="p-1"
+                    title="Move down">
                     <x-heroicon-o-arrow-down class="w-3 h-3 text-gray-500 hover:text-gray-900" />
                 </button>
             @endif
 
-            @if($indent)
-                <button x-init x-tooltip.raw="Indent" type="button" wire:click="indentItem('{{ $statePath }}')" class="p-1" title="Indent">
+            @if ($indent)
+                <button x-init
+                    x-tooltip.raw="Indent"
+                    type="button"
+                    wire:click="indentItem('{{ $statePath }}')"
+                    class="p-1"
+                    title="Indent">
                     <x-heroicon-o-arrow-right class="w-3 h-3 text-gray-500 hover:text-gray-900" />
                 </button>
             @endif
 
-            @if($nested)
-                <button x-init x-tooltip.raw="Dedent" type="button" wire:click="dedentItem('{{ $statePath }}')" class="p-1" title="Dedent">
+            @if ($nested)
+                <button x-init
+                    x-tooltip.raw="Dedent"
+                    type="button"
+                    wire:click="dedentItem('{{ $statePath }}')"
+                    class="p-1"
+                    title="Dedent">
                     <x-heroicon-o-arrow-left class="w-3 h-3 text-gray-500 hover:text-gray-900" />
                 </button>
             @endif
 
-            <button x-init x-tooltip.raw="Remove" type="button" wire:click="removeItem('{{ $statePath }}')" class="p-1" title="Remove">
+            <button x-init
+                x-tooltip.raw="Remove"
+                type="button"
+                wire:click="removeItem('{{ $statePath }}')"
+                class="p-1"
+                title="Remove">
                 <x-heroicon-o-trash class="w-3 h-3 text-danger-500 hover:text-danger-900" />
             </button>
         </div>
     </div>
 
     <div class="ml-3 space-y-2">
-        <div class="border-l border-gray-300 pl-3" wire:key="{{ $statePath }}-children">
-            @foreach($item['children'] as $uuid => $child)
-                <x-filament-navigation::nav-item :statePath="$statePath . '.children.' . $uuid" :item="$child" :moveUp="!$loop->first" :moveDown="!$loop->last" :indent="!$loop->first && $loop->count > 1" nested />
+        <div class="pl-3 border-l border-gray-300"
+            wire:key="{{ $statePath }}-children">
+            @foreach ($item['children'] as $uuid => $child)
+                <x-filament-navigation::nav-item :statePath="$statePath . '.children.' . $uuid"
+                    :item="$child"
+                    :moveUp="!$loop->first"
+                    :moveDown="!$loop->last"
+                    :indent="!$loop->first && $loop->count > 1"
+                    nested />
             @endforeach
         </div>
     </div>

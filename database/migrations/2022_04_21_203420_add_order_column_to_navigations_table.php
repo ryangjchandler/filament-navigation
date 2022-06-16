b92db995ee226996689c447e618ebb1ae345bc62
Patrick <privat@skyraptor.eu>
2022-06-16 19:10:15 +0200

    Create 2022_04_21_203420_add_order_column_to_navigations_table.php

---
+++ database/migrations/2022_04_21_203420_add_order_column_to_navigations_table.php
@@ -0,0 +1,35 @@
+
+
+
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('navigations', function (Blueprint $table) {
+            $table->unsignedTinyInteger('order')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('navigations', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+    }
+};

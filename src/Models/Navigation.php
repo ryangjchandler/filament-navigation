f79b07282caede85e59538c1820f17ad41902363
Patrick <privat@skyraptor.eu>
2022-06-16 19:07:51 +0200

    Update Navigation.php

--- src/Models/Navigation.php
+++ src/Models/Navigation.php
@@ -4,6 +4,8 @@ namespace RyanChandler\FilamentNavigation\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
 /**
  * @property string $handle
@@ -11,13 +13,18 @@ use Illuminate\Database\Eloquent\Model;
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  */
-class Navigation extends Model
+class Navigation extends Model implements Sortable
 {
-    use HasFactory;
+    use HasFactory, SortableTrait;
 
     protected $guarded = [];
 
     protected $casts = [
         'items' => 'json',
+    ];
+    
+    public $sortable = [
+        'order_column_name' => 'order',
+        'sort_when_creating' => true,
     ];
 }

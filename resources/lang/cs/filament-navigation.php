dbd79e8f0f35e45e76c8a4207bca156891a566fd
koupisbean <hi@jkopecky.cz>
2022-06-24 02:51:10 +0200

    Added czech translations

---
+++ resources/lang/cs/filament-navigation.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Odkaz',
+        'url' => 'URL/Adresa',
+        'target' => 'Cíl',
+        'name' => 'Název',
+        'items' => 'Položky',
+        'handle' => 'ID',
+        'created_at' => 'Vytvořeno',
+        'updated_at' => 'Upraveno',
+     ],
+
+    'select-options' => [
+        'same-tab' => 'Ve stejném okně',
+        'new-tab' => 'V novém okně'
+    ],
+
+    'items' => [
+        'empty' => 'Žádné položky',
+        'add-item' => 'Přidat položku',
+        'add-child' => 'Přidat dítě',
+        'move-up' => 'Posunout nahoru',
+        'move-down' => 'Posunout dolu',
+        'indent' => 'Přidělit',
+        'dedent' => 'Oddělit',
+        'remove' => 'Odstranit'
+    ],
+
+    'items-modal' => [
+        'title' => 'Nová položka',
+        'label' => 'Název',
+        'type' => 'Typ',
+        'btn' => 'Uložit',
+    ],
+];

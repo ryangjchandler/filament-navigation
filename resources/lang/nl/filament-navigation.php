3c50647d4f85a16c7628c96392fbf2f05cee91c1
Tally Schmeits <tally@schmeits.com>
2022-08-09 10:48:06 +0200

    Dutch translations

---
+++ resources/lang/nl/filament-navigation.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Externe link',
+        'url' => 'URL',
+        'target' => 'Doel',
+        'name' => 'Naam',
+        'items' => 'Navigatie items',
+        'handle' => 'Handle',
+        'created_at' => 'Gemaakt op',
+        'updated_at' => 'Bijgewerkt op',
+     ],
+
+    'select-options' => [
+        'same-tab' => 'Zelfde tab',
+        'new-tab' => 'Nieuw tab'
+    ],
+
+    'items' => [
+        'empty' => 'Geen items gevonden.',
+        'add-item' => 'Voeg nieuw item toe',
+        'add-child' => 'Voeg genest menuitem toe',
+        'move-up' => 'Verplaats omhoog',
+        'move-down' => 'Verplaats omlaag',
+        'indent' => 'Inspringen rechts',
+        'dedent' => 'Inspringen links',
+        'remove' => 'Verwijder',
+        'resource-url' => 'URL:',
+        'resource-url-tab' => '(openen in :name tab)',
+        'resource-url-tab-new' => 'nieuw',
+        'resource-url-tab-same' => 'zelfde',
+        'resource-id' => 'ID:',
+    ],
+
+    'items-modal' => [
+        'title' => 'Item',
+        'label' => 'Label',
+        'type' => 'Type',
+        'btn' => 'Opslaan',
+    ],
+];

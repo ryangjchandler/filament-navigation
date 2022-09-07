c10e7d09d04a703df7c73dc9a417b0ff6a3f798d
Matheus <matheus@bright.pt>
2022-09-07 13:18:26 +0100

    [feat] Add pt_BR and pt_PT translations

---
+++ resources/lang/pt_PT/filament-navigation.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Link externo',
+        'url'           => 'URL',
+        'target'        => 'Abrir em',
+        'name'          => 'Nome',
+        'items'         => 'Itens',
+        'handle'        => 'Referência',
+        'created_at'    => 'Criado em',
+        'updated_at'    => 'Editado em',
+
+    ],
+
+    'select-options' => [
+        'same-tab' => 'Mesmo separador',
+        'new-tab'  => 'Novo separador',
+    ],
+
+    'items' => [
+        'empty'     => 'Nenhum item',
+        'add-item'  => 'Adicionar item',
+        'add-child' => 'Adicionar subitem',
+        'move-up'   => 'Mover para cima',
+        'move-down' => 'Mover para baixo',
+        'indent'    => 'Avançar',
+        'dedent'    => 'Recuar',
+        'remove'    => 'Remover',
+    ],
+
+    'items-modal' => [
+        'title' => 'Item',
+        'label' => 'Nome',
+        'type'  => 'Tipo',
+        'btn'   => 'Guardar',
+    ],
+];

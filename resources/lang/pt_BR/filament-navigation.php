15fc12d70d3ad642aee45d6f20d737ebdda1871d
Matheus Machado <matheusdev@me.com>
2022-09-11 15:31:38 +0100

    [feat] Add pt_BR and pt_PT translations (#36)
    Co-authored-by: Matheus <matheus@bright.pt>

---
+++ resources/lang/pt_BR/filament-navigation.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Link externo',
+        'url'           => 'URL',
+        'target'        => 'Abrir em',
+        'name'          => 'Nome',
+        'items'         => 'Itens',
+        'handle'        => 'Referência',
+        'created_at'    => 'Criado em',
+        'updated_at'    => 'Editado em',
+
+    ],
+
+    'select-options' => [
+        'same-tab' => 'Mesma tab',
+        'new-tab'  => 'Nova tab',
+    ],
+
+    'items' => [
+        'empty'     => 'Nenhum item',
+        'add-item'  => 'Adicionar item',
+        'add-child' => 'Adicionar subitem',
+        'move-up'   => 'Mover para cima',
+        'move-down' => 'Mover para baixo',
+        'indent'    => 'Avançar',
+        'dedent'    => 'Recuar',
+        'remove'    => 'Remover',
+    ],
+
+    'items-modal' => [
+        'title' => 'Item',
+        'label' => 'Nome',
+        'type'  => 'Tipo',
+        'btn'   => 'Salvar',
+    ],
+];

c59bb6946394f01e12afa10ecf2af71a1df520ff
Patrick <privat@skyraptor.eu>
2022-09-11 16:33:07 +0200

    Add german translations (#35)

---
+++ resources/lang/de/filament-navigation.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Externer Link',
+        'url' => 'URL',
+        'target' => 'Ziel',
+        'name' => 'Name',
+        'items' => 'Einträge',
+        'handle' => 'Handle',
+        'created_at' => 'Erstellt',
+        'updated_at' => 'Geändert',
+
+     ],
+
+    'select-options' => [
+        'same-tab' => 'Gleicher Tab',
+        'new-tab' => 'Neuer Tab'
+    ],
+
+    'items' => [
+        'empty' => 'Keine Einträge.',
+        'add-item' => 'Einträge hinzufügen',
+        'add-child' => 'Kind hinzufügen',
+        'move-up' => 'Nach oben verschieben',
+        'move-down' => 'Nach unten verschieben',
+        'indent' => 'Einziehen',
+        'dedent' => 'Ausziehen',
+        'remove' => 'Entfernen'
+    ],
+
+    'items-modal' => [
+        'title' => 'Eintrag',
+        'label' => 'Name',
+        'type' => 'Typ',
+        'btn' => 'Speichern',
+    ],
+];

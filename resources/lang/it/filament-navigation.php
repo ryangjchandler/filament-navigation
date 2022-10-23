38fa3a410448065dc35046ec2d0d808c314d947c
carlo <carlo.gherarducci@gmail.com>
2022-10-23 11:34:29 +0200

    Italian tranlation; fixed label 'Type' in modal

---
+++ resources/lang/it/filament-navigation.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Link esterno',
+        'url' => 'URL',
+        'target' => 'Apri in',
+        'name' => 'Nome',
+        'items' => 'Voci di menù',
+        'handle' => 'Riferimento',
+        'created_at' => 'Data di creazione',
+        'updated_at' => 'Data di aggiornamento',
+
+    ],
+
+    'select-options' => [
+        'same-tab' => 'Stessa scheda',
+        'new-tab' => 'Nuova scheda'
+    ],
+
+    'items' => [
+        'empty' => 'Nesuna voce di menù.',
+        'add-item' => 'Aggiungi voce di menù',
+        'add-child' => 'Aggiungi sotto-voce di menù',
+        'move-up' => 'Sposta su',
+        'move-down' => 'Sposta giù',
+        'indent' => 'Aumenta rientro',
+        'dedent' => 'Riduci rientro',
+        'remove' => 'Rimuovi'
+    ],
+
+    'items-modal' => [
+        'title' => 'Voce di menù',
+        'label' => 'Etichetta',
+        'type' => 'Tipo',
+        'btn' => 'Salva',
+    ],
+];

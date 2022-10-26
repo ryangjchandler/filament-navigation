11efe2735035b1728ce9dadc7513e76aa301e5aa
Fabien Karsegard <fabien@karsegard.ch>
2022-10-26 19:22:50 +0200

    Added French translations

---
+++ resources/lang/fr/filament-navigation.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Lien externe',
+        'url' => 'URL',
+        'target' => 'Cible',
+        'name' => 'Nom',
+        'items' => 'Éléments',
+        'handle' => 'Clé',
+        'created_at' => 'Créé le',
+        'updated_at' => 'Màj le',
+
+     ],
+
+    'select-options' => [
+        'same-tab' => 'Même fenêtre',
+        'new-tab' => 'Nouvelle fenêtre'
+    ],
+
+    'items' => [
+        'empty' => 'pas d\'élément',
+        'add-item' => 'Ajouter un élément',
+        'add-child' => 'Ajouter un enfant',
+        'move-up' => 'Déplacer vers le haut',
+        'move-down' => 'Déplacer vers le bas',
+        'indent' => 'Indenter',
+        'dedent' => 'Désindenter',
+        'remove' => 'Supprimer'
+    ],
+
+    'items-modal' => [
+        'title' => 'Élément',
+        'label' => 'Label',
+        'type' => 'Type',
+        'btn' => 'Enregistrer',
+    ],
+];

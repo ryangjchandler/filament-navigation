61cdf1ed37529fd576f100e0ad1abebd56bcd36f
Sten Govaerts <sten.govaerts@gmail.com>
2023-03-16 21:43:35 +0100

    Dutch translations (#68)

---
+++ resources/lang/nl/filament-navigation.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Externe link',
+        'url' => 'URL',
+        'target' => 'Doel',
+        'name' => 'Naam',
+        'items' => 'Elementen',
+        'handle' => 'Sleutel',
+        'created_at' => 'Aangemaakt op',
+        'updated_at' => 'Aangepast op',
+
+     ],
+
+    'select-options' => [
+        'same-tab' => 'Dezelfde tab',
+        'new-tab' => 'Nieuwe tab'
+    ],
+
+    'items' => [
+        'empty' => 'Geen elementen.',
+        'add-item' => 'Nieuw element',
+        'add-child' => 'Voeg kind toe',
+        'move-up' => 'Verplaats omhoog',
+        'move-down' => 'Verplaats omlaag',
+        'indent' => 'Inspringen',
+        'dedent' => 'Terugspringen',
+        'remove' => 'Verwijderen'
+    ],
+
+    'items-modal' => [
+        'title' => 'Element',
+        'label' => 'Label',
+        'type' => 'Type',
+        'btn' => 'Bewaar',
+    ],
+];

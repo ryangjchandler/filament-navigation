1197feeb398d9eb01719b0529643dcaee3d3f0a6
Daniel Gurev <doodka.dood@gmail.com>
2023-03-28 16:49:40 +0300

    Add Russian translation

---
+++ resources/lang/ru/filament-navigation.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Внешняя ссылка',
+        'url' => 'URL',
+        'target' => 'Открыть в',
+        'name' => 'Название',
+        'items' => 'Элементы',
+        'handle' => 'Идентификатор',
+        'created_at' => 'Создано в',
+        'updated_at' => 'Изменено в',
+    ],
+
+    'select-options' => [
+        'same-tab' => 'Этой вкладке',
+        'new-tab' => 'Новой вкладке'
+    ],
+
+    'items' => [
+        'empty' => 'Нет элементов',
+        'add-item' => 'Добавить элемент',
+        'add-child' => 'Добавить дочерний',
+        'move-up' => 'Переместить выше',
+        'move-down' => 'Переместить ниже',
+        'indent' => 'Indent',
+        'dedent' => 'Dedent',
+        'remove' => 'Удалить'
+    ],
+
+    'items-modal' => [
+        'title' => 'Элемент',
+        'label' => 'Название',
+        'type' => 'Тип',
+        'btn' => 'Сохранить',
+    ],
+];

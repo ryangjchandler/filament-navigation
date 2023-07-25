48102915386f917679f9f730be5487741a801fa0
Ash Monsh <wh7r.com@gmail.com>
2023-07-25 03:34:50 +0300

    remove config

--- config/filament-navigation.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config for RyanChandler/FilamentNavigation
-return [
-
-    'navigation_model' => \RyanChandler\FilamentNavigation\Models\Navigation::class,
-
-    'navigation_resource' => \RyanChandler\FilamentNavigation\Filament\Resources\NavigationResource::class,
-];

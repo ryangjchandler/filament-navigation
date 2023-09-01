6ab59c235fab3e397eff68b8d4f735c754aa2b14
Alejandro Vásquez Neira <alejandro.vasquez@neurotools.cl>
2023-09-01 18:16:43 -0400

    Spanish translation

---
+++ resources/lang/es/filament-navigation.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'attributes' => [
+        'external-link' => 'Vínculo externo',
+        'url' => 'URL',
+        'target' => 'Destino',
+        'name' => 'Nombre',
+        'items' => 'Ítems',
+        'handle' => 'Referencia',
+        'created_at' => 'Creado',
+        'updated_at' => 'Actualizado',
+
+     ],
+
+    'select-options' => [
+        'same-tab' => 'Misma pestaña',
+        'new-tab' => 'Nueva pestaña'
+    ],
+
+    'items' => [
+        'empty' => 'No hay ítems.',
+        'add-item' => 'Agregar ítem',
+        'add-child' => 'Agregar hijo',
+        'move-up' => 'Mover arriba',
+        'move-down' => 'Mover abajo',
+        'indent' => 'Indentar',
+        'dedent' => 'Desindentar',
+        'remove' => 'Remover'
+    ],
+
+    'items-modal' => [
+        'title' => 'Ítem',
+        'label' => 'Etiqueta',
+        'type' => 'Tipo',
+        'btn' => 'Guardar',
+    ],
+];

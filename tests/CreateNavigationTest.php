5cebaa0d54ce8f0451f0ec26c59f3c98218ca6af
ryangjchandler <ryangjchandler@users.noreply.github.com>
2025-02-24 19:11:24 +0000

    Fix styling

--- tests/CreateNavigationTest.php
+++ tests/CreateNavigationTest.php
@@ -6,7 +6,6 @@ use Pest\Expectation;
 use function Pest\Laravel\assertDatabaseHas;
 
 use RyanChandler\FilamentNavigation\Filament\Resources\NavigationResource\Pages\CreateNavigation;
-
 use RyanChandler\FilamentNavigation\Models\Navigation;
 
 it('can create a navigation menu', function () {
